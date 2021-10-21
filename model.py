5880bb3104d37cea028e630f36c8dbe63bad157b
zcfei <3300949577@qq.com>
2021-10-21 20:04:15 +0800

    update

--- model.py
+++ model.py
@@ -1,1 +1,27 @@
-from transformers import * 
+# coding=utf-8 
+# from __future__ import * 
+
+import copy 
+import math 
+import logging 
+import numpy as np 
+
+import torch
+import torch.nn.functional as F 
+from torch.nn.modules.loss import _Loss 
+from transformers.modeling_utils import PreTrainedModel 
+
+from configuration import PVTConfig 
+from transformers.modeling_bert import load_tf_weights_in_bert, BertPooler, BertIntermediate, \
+    BertOutput, BertPredictionHeadTransform, BertSelfOutput, BertLMPredictionHead, BertOnlyMLMHead, \
+    BertOnlyMLMHead, BertEmbeddings, BertOnlyNSPHead
+
+
+logging.basicConfig(level=logging.INFO) 
+logger = logging.getLogger(__name__)
+
+BertLayerNorm = torch.nn.LayerNorm 
+
+
+
+
---
+++ train.py
@@ -0,0 +1,17 @@
+# coding=utf-8 
+
+import os 
+import logging 
+import json 
+import argparse 
+
+
+import numpy as np 
+
+
+
+def main(): 
+    print('aa')
+
+if __name__ == '__main__':
+    main()
